4b431c203f988de9ae1981a521ac3a3194514d1d
Kemal Sivro <kemalsivro@gmail.com>
2023-10-24 18:40:13 +0200

    first

---
+++ src/app/example-tasks.ts
@@ -0,0 +1,21 @@
+import { Task } from './Task';
+export const TASKS: Task[] = [
+	{
+		id: 1,
+		text: 'Doctors Appointment',
+		day: 'Oct 26th at 11:00am',
+		reminder: true,
+	},
+	{
+		id: 2,
+		text: 'Work Meeting',
+		day: 'Oct 25th at 9:00am',
+		reminder: true,
+	},
+	{
+		id: 3,
+		text: 'Grocery Shopping',
+		day: 'Oct 25th at 3:00pm',
+		reminder: false,
+	},
+];
